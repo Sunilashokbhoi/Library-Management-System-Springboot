b66564aaf6c609565f223fb5e68415fe2c684668
Sunilashokbhoi <sunilbhoi606@gmail.com>
2024-02-26 22:31:07 +0530

    MODIFIED: Create MemberNotFoundException

---
+++ src/main/java/com/infogalaxy/librarymanagementsystem/exception/MemberNotFounfException.java
@@ -0,0 +1,8 @@
+package com.infogalaxy.librarymanagementsystem.exception;
+
+
+public class MemberNotFounfException extends RuntimeException{
+    public MemberNotFounfException(String message){
+        super(message);
+    }
+}
