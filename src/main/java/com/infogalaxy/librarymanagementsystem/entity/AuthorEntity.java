bad7fb4563d3ab85bab46d32ec6858878b9cc22a
Sunilashokbhoi <sunilbhoi606@gmail.com>
2024-02-27 10:23:33 +0530

    MODIFIED: Create Repo layer for Sql Queries Use

--- src/main/java/com/infogalaxy/librarymanagementsystem/entity/AuthorEntity.java
+++ src/main/java/com/infogalaxy/librarymanagementsystem/entity/AuthorEntity.java
@@ -2,11 +2,12 @@ package com.infogalaxy.librarymanagementsystem.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
-
+@Table(name ="librarymemberentity")
 public class AuthorEntity {
     @Id
     private int authirid;
